11170be736485239e9e10c53dfecae4a8dabe9b9
Tarun <tarun.rana1999@gmail.com>
2023-11-15 02:01:38 +0530

    Login done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  const[isLoggedIn, setLoggedIn]=useState(false);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+        <login isLoggedIn={isLoggedIn} setLoggedIn={setLoggedIn}/>
     </div>
   )
 }
---
+++ src/components/login.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const login = (isLoggedIn, setLoggedIn) => {
+    const [data, setData]= useState({username: '', password:''});
+
+    const handleSubmit=(e)=>{
+        setData({...data, [e.target.name]: e.target.value});
+    };
+
+  return (
+    <div>
+        {isLoggedIn ? (<p>You are logged in!</p>):
+        (
+            <form>
+        <div>
+            <label htmlFor="username">Username</label>
+            <input type="text" id='username' placeholder='Username' value={data.username} onChange={handleSubmit} />
+        </div>
+        <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" id='password' placeholder='Password' value={data.password} onChange={handleSubmit} />
+        </div>
+        <button onClick={()=>setLoggedIn('true')}>Login</button>
+        </form>
+        )}
+    </div>
+  )
+}
+
+export default login
