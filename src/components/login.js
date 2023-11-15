d7649d650a839273d7fa589e972e58861242a906
Tarun <tarun.rana1999@gmail.com>
2023-11-16 00:35:28 +0530

    Changes

--- src/components/login.js
+++ src/components/login.js
@@ -5,6 +5,7 @@ const login = (isLoggedIn, setLoggedIn) => {
     const [data, setData]= useState({username: '', password:''});
 
     const handleSubmit=(e)=>{
+        e.preventDefault();
         setData({...data, [e.target.name]: e.target.value});
     };
 
@@ -14,12 +15,12 @@ const login = (isLoggedIn, setLoggedIn) => {
         (
             <form>
         <div>
-            <label htmlFor="username">Username</label>
-            <input type="text" id='username' placeholder='Username' value={data.username} onChange={handleSubmit} />
+            <label htmlFor="username">Username:</label>
+            <input type="text" id='username' name='username' placeholder='Username' value={data.username} onChange={handleSubmit} />
         </div>
         <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" id='password' placeholder='Password' value={data.password} onChange={handleSubmit} />
+            <label htmlFor="password">Password:</label>
+            <input type="password" id='password' name='password' placeholder='Password' value={data.password} onChange={handleSubmit} />
         </div>
         <button onClick={()=>setLoggedIn('true')}>Login</button>
         </form>
